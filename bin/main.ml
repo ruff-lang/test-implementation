8fa342bf9a98ced6d9904be334ce30fe0a9be28e
rgrmrts <omar@bricck.co>
2020-11-10 20:37:57 -0500

    use concatenated string and use spaces instead of tab

--- bin/main.ml
+++ bin/main.ml
@@ -1,23 +1,28 @@
 (* Main program entrypoint for Bunny. *)
 
-(* String to print out for help or when no arguments are given. *)
-let help_string =
-  "The Bunny Programming Language 🐰\n\n\
-   usage: bunny <command> <arguments>\n\n\
-   The most commonly used bunny commands are:\n\
-   \tbuild     Compiles the current project to bytecode.\n\
-   \tcarrot    Interact with the carrot system to add or remove libraries.\n\
-   \tdoc       Use the documentation generator tool.\n\
-   \texec      Execute an installed binary carrot (program).\n\
-   \tformat    Format the current project, provided directory (recursively), or file.\n\
-   \thelp      Print this help text.\n\
-   \tnew       Create a new project or carrot skeleton.\n\
-   \trepl      Launch an interactive REPL for Bunny.\n\
-   \trun       Run a file containing Bunny code.\n\
-   \ttest      Runs all or provided tests for the current project.\n\
-   \tversion   Prints the current version of Bunny.\n\n\
-   To get more usage information on a specific command:\n\
-   \tbunny help <command>\n"
+(* String to print when running bunny without any specified command. *)
+let short_help =
+  "Usage: bunny <command> <argument>\n\n"
+  ^ "To get more usage information run 'bunny help'\n"
+
+(* String to print when runny the bunny help command. *)
+let full_help =
+  "The Bunny Programming Language 🐰\n\n"
+  ^ "Usage: bunny <command> <arguments>\n\n"
+  ^ "The most commonly used bunny commands are:\n"
+  ^ "    build     Compiles the current project to bytecode.\n"
+  ^ "    carrot    Interact with the carrot system to add or remove libraries.\n"
+  ^ "    doc       Use the documentation generator tool.\n"
+  ^ "    exec      Execute an installed binary carrot (program).\n"
+  ^ "    format    Format the current project, provided directory (recursively), or file.\n"
+  ^ "    help      Print this help text.\n"
+  ^ "    new       Create a new project or carrot skeleton.\n"
+  ^ "    repl      Launch an interactive REPL for Bunny.\n"
+  ^ "    run       Run a file containing Bunny code.\n"
+  ^ "    test      Runs all or provided tests for the current project.\n"
+  ^ "    version   Prints the current version of Bunny.\n\n"
+  ^ "To get more usage information on a specific command:\n"
+  ^ "    bunny help <command>\n"
 
 (* Main program entrypoint, parses the arguments and dispatches the appropriate module. *)
-let () = Printf.printf "%s" help_string
+let () = Printf.printf "%s" full_help
