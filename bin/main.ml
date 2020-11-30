4e3330e9ef75dbe9cbb690c5a6835e7629bd606e
rgrmrts <omar@bricck.co>
2020-11-30 18:23:02 -0500

    remove dots

--- bin/main.ml
+++ bin/main.ml
@@ -10,20 +10,19 @@ let short_help =
 
 (* String to print when runny the bunny help command. *)
 let full_help =
-  "The Bunny Programming Language 🐰\n\n\
-   Usage: bunny <command> <arguments>\n\n\
+  "Usage: bunny <command> <arguments>\n\n\
    Some of the most commonly used bunny commands are:\n\
-  \    build     Compiles the current project to bytecode.\n\
-  \    carrot    Interact with the carrot system to add or remove libraries.\n\
-  \    doc       Use the documentation generator tool.\n\
-  \    exec      Execute an installed binary carrot (program).\n\
-  \    format    Format the current project, provided directory (recursively), or file.\n\
-  \    help      Print this help text.\n\
-  \    new       Create a new project or carrot skeleton.\n\
-  \    repl      Launch an interactive REPL for Bunny.\n\
-  \    run       Run a file containing Bunny code.\n\
-  \    test      Runs all or provided tests for the current project.\n\
-  \    version   Prints the current version of Bunny.\n\n\
+  \    build     Compiles the current project to bytecode\n\
+  \    carrot    Interact with the carrot system to add or remove libraries\n\
+  \    doc       Use the documentation generator tool\n\
+  \    exec      Execute an installed binary carrot (program)\n\
+  \    format    Format the current project, provided directory (recursively), or file\n\
+  \    help      Print this help text\n\
+  \    new       Create a new project or carrot skeleton\n\
+  \    repl      Launch an interactive REPL for Bunny\n\
+  \    run       Run a file containing Bunny code\n\
+  \    test      Runs all or provided tests for the current project\n\
+  \    version   Prints the current version of Bunny\n\n\
    To get more usage information on a specific command:\n\
   \    bunny help <command>\n"
 
