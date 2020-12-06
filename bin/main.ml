4d693bdc380b925512931f1472be2d445fc6e6b2
rgrmrts <omar@bricck.co>
2020-12-06 11:11:25 -0500

    back to use concats

--- bin/main.ml
+++ bin/main.ml
@@ -6,30 +6,23 @@
 
 (* String to print when running bunny without any specified command. *)
 let short_help =
-  "\x1B[1mUsage:\x1B[0m bunny <command> <argument>\n\n\
-   To get more usage information run 'bunny help'\n"
+  "Usage: bunny <command> <argument>\n\n" ^ "To get more usage information run 'bunny help'\n"
 
-(*
- * String to print when runny the bunny help command.
- * "\x1B[1m" starts bolding text for Printf
- * "\x1B[0m" finishing bolding text for Printf
- *)
+(* String to print when runny the bunny help command. *)
 let full_help =
-  "\x1B[1mUsage:\x1B[0m bunny <command> <arguments>\n\n\
-   \x1B[1mSome of the most commonly used bunny commands are:\x1B[0m\n\
-  \    build     Compiles the current project to bytecode\n\
-  \    carrot    Interact with the carrot system to add or remove libraries\n\
-  \    doc       Use the documentation generator tool\n\
-  \    exec      Execute an installed binary carrot (program)\n\
-  \    format    Format the current project, directory (recursively), or file\n\
-  \    help      Print this help text\n\
-  \    new       Create a new project or carrot skeleton\n\
-  \    repl      Launch an interactive REPL for Bunny\n\
-  \    run       Run a file containing Bunny code\n\
-  \    test      Runs all or provided tests for the current project\n\
-  \    version   Prints the current version of Bunny\n\n\
-   \x1B[1mTo get more usage information on a specific command:\x1B[0m\n\
-  \    bunny help <command>\n"
+  "Usage: bunny <command> <arguments>\n\n" ^ "Some of the most commonly used bunny commands are:\n"
+  ^ "    build     Compiles the current project to bytecode.\n"
+  ^ "    carrot    Interact with the carrot system to add or remove libraries.\n"
+  ^ "    doc       Use the documentation generator tool.\n"
+  ^ "    exec      Execute an installed binary carrot (program).\n"
+  ^ "    format    Format the current project, provided directory (recursively), or file.\n"
+  ^ "    help      Print this help text.\n"
+  ^ "    new       Create a new project or carrot skeleton.\n"
+  ^ "    repl      Launch an interactive REPL for Bunny.\n"
+  ^ "    run       Run a file containing Bunny code.\n"
+  ^ "    test      Runs all or provided tests for the current project.\n"
+  ^ "    version   Prints the current version of Bunny.\n\n"
+  ^ "To get more usage information on a specific command:\n" ^ "    bunny help <command>\n"
 
 let () =
   let number_of_arguments = Array.length Sys.argv in
