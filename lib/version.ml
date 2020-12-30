1d844ef81b60aec0476b83d34b6ea282c1894659
rgrmrts <omar@bricck.co>
2020-12-29 21:53:45 -0500

    initialize rust version

--- lib/version.ml
+++
@@ -1,9 +0,0 @@
-(* Increment version for every new version of Bunny. This is only used for printing. *)
-
-open Printf
-
-let print_version () =
-  let version = "v0.0.1" in
-  let url = "https://bunny-lang.org" in
-  let string = "Bunny Programming Language" in
-  printf "%s\nVersion: %s\n%s\n" string version url
--- lib/virtual_machine.ml
+++
@@ -1,6 +0,0 @@
-(*
- * Virtual Machine
- *
- * The primary runtime execution environment for bunny applications. The virtual
- * machine consumes bytecode and computes things.
- *)
